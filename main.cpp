8cbca90aed520db996ce7523eb0fae6ce389a33a
Pawan <pawanjbs5@gmail.com>
2023-12-27 17:24:24 +0530

    More refactoring and play/pause text

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <cstdio>
+#include <cstring>
 #include <raylib.h>
 #include <vector>
 
@@ -5,11 +7,6 @@ typedef struct {
   char *file_path;
   Music music;
 } Track;
-
-typedef struct {
-  std::vector<Track> tracks;
-  int current_track;
-} PlayerState;
 
 int main() {
   int screenWidth = 800;
@@ -21,66 +18,77 @@ int main() {
 
   float timePlayed = 0.0f;
   bool pause = false;
-  PlayerState player_state = PlayerState();
-  player_state.current_track = -1;
+  std::vector<Track> tracks;
+  int current_track = -1;
+  Track *cur_track = NULL;
 
   while (!WindowShouldClose()) {
     if (IsFileDropped()) {
       FilePathList droppedFiles = LoadDroppedFiles();
       for (int i = 0; i < droppedFiles.count; i++) {
-        char *file_name = droppedFiles.paths[i];
+        char *file_name = strdup(droppedFiles.paths[i]);
         Music music = LoadMusicStream(file_name);
         if (IsMusicReady(music)) {
-          player_state.tracks.push_back(Track{file_name, music});
+          tracks.push_back(Track{file_name, music});
         }
       }
       UnloadDroppedFiles(droppedFiles);
 
-      if (player_state.tracks.size() > 0) {
-        player_state.current_track = 0;
-        Track cur_track = player_state.tracks.at(player_state.current_track);
-        PlayMusicStream(cur_track.music);
+      if (tracks.size() > 0) {
+        current_track = 0;
+        cur_track = &tracks.at(current_track);
+        PlayMusicStream(cur_track->music);
       }
     }
 
-    if (player_state.current_track >= 0) {
-      Track cur_track = player_state.tracks.at(player_state.current_track);
-      Music cur_music = cur_track.music;
-
-      float track_length = GetMusicTimeLength(cur_music);
-      float track_played = GetMusicTimePlayed(cur_music);
+    if (tracks.size() > current_track) {
+      cur_track = &tracks.at(current_track);
+      Music music = cur_track->music;
+      float track_length = GetMusicTimeLength(music);
+      float track_played = GetMusicTimePlayed(music);
       timePlayed = track_played / track_length;
       if (timePlayed > 1.0f)
         timePlayed = 1.0f;
 
       if (track_played >= track_length - 0.3) {
-        if (player_state.current_track < player_state.tracks.size()) {
-          player_state.current_track += 1;
-          cur_track = player_state.tracks.at(player_state.current_track);
-          PlayMusicStream(cur_track.music);
+        current_track += 1;
+        if (current_track < tracks.size()) {
+          cur_track = &tracks.at(current_track);
+          PlayMusicStream(cur_track->music);
         } else
-          StopMusicStream(cur_music);
+          StopMusicStream(music);
       }
-
-      UpdateMusicStream(cur_music);
+      UpdateMusicStream(music);
 
       if (IsKeyPressed(KEY_R)) {
-        StopMusicStream(cur_music);
-        PlayMusicStream(cur_music);
+        StopMusicStream(music);
+        PlayMusicStream(music);
       }
 
       if (IsKeyPressed(KEY_SPACE)) {
-        if (IsMusicStreamPlaying(cur_music))
-          PauseMusicStream(cur_music);
+        if (IsMusicStreamPlaying(music))
+          PauseMusicStream(music);
         else
-          ResumeMusicStream(cur_music);
+          ResumeMusicStream(music);
       }
     }
 
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
-    DrawText("MUSIC SHOULD BE PLAYING!", 255, 150, 20, LIGHTGRAY);
+    if (tracks.size() > 0) {
+      for (int i = 0; i < tracks.size(); i++) {
+        const char *file_name = GetFileName(tracks[i].file_path);
+        DrawText(file_name, 255, 0 + i * 20, 20, LIGHTGRAY);
+      }
+    }
+
+    if (cur_track != NULL) {
+      const char *file_name = GetFileName(cur_track->file_path);
+      DrawText(file_name, 255, 100, 20, LIGHTGRAY);
+      DrawText(IsMusicStreamPlaying(cur_track->music) ? "Playing" : "Paused",
+               255, 120, 20, LIGHTGRAY);
+    }
     DrawRectangle(200, 200, 400, 12, LIGHTGRAY);
     DrawRectangle(200, 200, (int)(timePlayed * 400.0f), 12, MAROON);
     DrawRectangleLines(200, 200, 400, 12, GRAY);
