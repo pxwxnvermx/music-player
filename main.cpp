72b2b0ff5d25444b5d972261a13f4a5f5a318ca6
Pawan <pawanjbs5@gmail.com>
2023-12-31 17:02:09 +0530

    Adding coords for controls

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <raylib.h>
+#include <string>
 #include <vector>
 
 typedef struct {
@@ -19,20 +20,19 @@ bool DrawButton(Vector2 bounds, const char *text, Color color) {
   rect_bounds.height = text_bounds.height + 10;
   text_bounds.x += (rect_bounds.width - text_bounds.width) * 0.5;
   text_bounds.y += (rect_bounds.height - text_bounds.height) * 0.5;
-  DrawRectangleLinesEx(rect_bounds, 2, color);
-  DrawText(text, text_bounds.x, text_bounds.y, 20, color);
 
   Vector2 mousePoint = GetMousePosition();
   if (CheckCollisionPointRec(mousePoint, rect_bounds)) {
-    if (CheckCollisionPointRec(mousePoint, rect_bounds)) {
-      DrawRectangle(rect_bounds.x, rect_bounds.y + rect_bounds.height,
-                    rect_bounds.width, 1, color);
-      DrawRectangle(rect_bounds.x + rect_bounds.width, rect_bounds.y, 1,
-                    rect_bounds.height, color);
-      if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
-        result = true;
-    }
+    Color dark_color = ColorBrightness(color, 0.1f);
+    DrawRectangle(rect_bounds.x, rect_bounds.y + rect_bounds.height,
+                  rect_bounds.width + 2, 2, dark_color);
+    DrawRectangle(rect_bounds.x + rect_bounds.width, rect_bounds.y, 2,
+                  rect_bounds.height + 2, dark_color);
+    if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+      result = true;
   }
+  DrawRectangleLinesEx(rect_bounds, 2, color);
+  DrawText(text, text_bounds.x, text_bounds.y, 20, color);
   return result;
 }
 
@@ -43,10 +43,25 @@ bool pp_music(Music music) {
   return music_playing;
 }
 
+int change_track(std::vector<Track> *tracks, int current_track,
+                 int changed_track) {
+  Track cur_track = tracks->at(current_track);
+  if (0 <= changed_track && changed_track < tracks->size()) {
+    StopMusicStream(cur_track.music);
+    cur_track = tracks->at(current_track);
+    PlayMusicStream(cur_track.music);
+    return changed_track;
+  }
+  StopMusicStream(cur_track.music);
+  return current_track;
+}
+
 int main() {
-  int screenWidth = 800;
-  int screenHeight = 600;
-  InitWindow(screenWidth, screenHeight, "Something");
+  int factor = 80;
+  int screenWidth = factor * 16;
+  int screenHeight = factor * 9;
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+  InitWindow(screenWidth, screenHeight, "Music Player");
   InitAudioDevice();
   SetTargetFPS(60);
   SetTraceLogLevel(LOG_ALL);
@@ -57,9 +72,7 @@ int main() {
   int current_track = -1;
   Track *cur_track = NULL;
 
-  // play paused text longest is PLAYING which is size 8 with \O
-  char *pp_btn_text = (char *)malloc(8);
-  strcpy(pp_btn_text, "PAUSED");
+  std::string pp_btn_text = "PAUSED";
 
   while (!WindowShouldClose()) {
     if (IsFileDropped()) {
@@ -111,27 +124,53 @@ int main() {
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
+
     if (tracks.size() > 0) {
-      for (int i = 0; i < tracks.size(); i++) {
-        const char *file_name = GetFileName(tracks[i].file_path);
-        DrawText(file_name, 255, 0 + i * 20, 20, LIGHTGRAY);
-      }
+      for (int i = 0; i < tracks.size(); i++)
+        DrawText(GetFileName(tracks[i].file_path), 255, 0 + i * 20, 20,
+                 LIGHTGRAY);
     }
+
+    // PREV 76
+    // PAUSED 102
+    // NEXT 76
+    // 262 full width of controls with padding for first two button
+    float controls_x = (screenWidth - 262) * 0.5;
+    float controls_y = (screenHeight - 115) * 0.75;
+    bool prev_btn_clicked = DrawButton(
+        Vector2{.x = controls_x, .y = controls_y}, "PREV", LIGHTGRAY);
+    bool pp_btn_clicked =
+        DrawButton(Vector2{.x = controls_x + 80, .y = controls_y},
+                   pp_btn_text.c_str(), LIGHTGRAY);
+    bool next_btn_clicked = DrawButton(
+        Vector2{.x = controls_x + 186, .y = controls_y}, "NEXT", LIGHTGRAY);
 
     if (cur_track != NULL) {
       const char *file_name = GetFileName(cur_track->file_path);
-      DrawText(file_name, 255, 100, 20, LIGHTGRAY);
+      float file_name_width = MeasureText(file_name, 20);
+      DrawText(file_name, (screenWidth - file_name_width) * 0.5,
+               controls_y - 25, 20, LIGHTGRAY);
+
+      if (pp_btn_clicked) {
+        if (pp_music(cur_track->music))
+          pp_btn_text = "PLAYING";
+        else
+          pp_btn_text = "PAUSED";
+      }
+
+      if (prev_btn_clicked)
+        current_track = change_track(&tracks, current_track, current_track - 1);
+
+      if (next_btn_clicked)
+        current_track = change_track(&tracks, current_track, current_track + 1);
     }
 
-    if (DrawButton(Vector2{.x = 245, .y = 115}, pp_btn_text, LIGHTGRAY) &&
-        cur_track != NULL)
-      pp_music(cur_track->music) ? strcpy(pp_btn_text, "PLAYING")
-                                 : strcpy(pp_btn_text, "PAUSED");
-    DrawRectangle(200, 200, 400, 12, LIGHTGRAY);
-    DrawRectangle(200, 200, (int)(timePlayed * 400.0f), 12, MAROON);
-    DrawRectangleLines(200, 200, 400, 12, GRAY);
-    DrawText("PRESS R TO RESTART MUSIC", 215, 250, 20, LIGHTGRAY);
-    DrawText("PRESS SPACE TO PAUSE/RESUME MUSIC", 208, 280, 20, LIGHTGRAY);
+    DrawRectangle((screenWidth - 400) * 0.5, controls_y - 60, 400, 12,
+                  LIGHTGRAY);
+    DrawRectangle((screenWidth - 400) * 0.5, controls_y - 60,
+                  (int)(timePlayed * 400.0f), 12, MAROON);
+    DrawRectangleLines((screenWidth - 400) * 0.5, controls_y - 60, 400, 12,
+                       GRAY);
 
     EndDrawing();
   }
